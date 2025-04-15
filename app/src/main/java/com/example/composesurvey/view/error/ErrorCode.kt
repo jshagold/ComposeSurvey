0bfebaccd23cf9378bcf593d129d2c7d69af7ae3
hakee <jshagold@gmail.com>
2025-04-15 16:08:54 +0900

    feat: ErrorCode 및 compose extension추가

--- app/src/main/java/com/example/composesurvey/view/error/ErrorCode.kt
+++ app/src/main/java/com/example/composesurvey/view/error/ErrorCode.kt
@@ -4,4 +4,5 @@ enum class ErrorCode(val errorName: String, val code: Int) {
     NONE(errorName = "", code = -1),
     UNEXPECTED(errorName = "예외 에러", code = 0),
     FILE(errorName = "파일 에러", code = 1),
+    NULLELEMENT(errorName = "항목이 없습니다.", code = 2)
 }
