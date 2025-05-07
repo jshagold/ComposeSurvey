42f56903fe0ddc92fb59eca93e343b024f81fd9f
hakee <jshagold@gmail.com>
2025-05-07 14:10:06 +0900

    feat: 로그 적용

--- app/src/main/java/com/example/composesurvey/view/app/ComposeSurveyApplication.kt
+++ app/src/main/java/com/example/composesurvey/view/app/ComposeSurveyApplication.kt
@@ -2,7 +2,14 @@ package com.example.composesurvey.view.app
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import io.github.aakira.napier.DebugAntilog
+import io.github.aakira.napier.Napier
 
 @HiltAndroidApp
 class ComposeSurveyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        Napier.base(DebugAntilog())
+    }
 }
--- app/src/main/java/com/example/composesurvey/view/error/ErrorCode.kt
+++ app/src/main/java/com/example/composesurvey/view/error/ErrorCode.kt
@@ -4,5 +4,6 @@ enum class ErrorCode(val errorName: String, val code: Int) {
     NONE(errorName = "", code = -1),
     UNEXPECTED(errorName = "예외 에러", code = 0),
     FILE(errorName = "파일 에러", code = 1),
-    NULLELEMENT(errorName = "항목이 없습니다.", code = 2)
+    NULL_ELEMENT(errorName = "항목이 없습니다.", code = 2),
+    DB(errorName = "DB 에러", code = 3),
 }
